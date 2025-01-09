f6bd356590973b07129d1ae787fbc94536202bdb
Agantuk Sarkar <agantuksarkar124@gmail.com>
2025-01-09 14:32:49 +0530

    update header
    add: provided nav logo

--- components/navBar.js
+++ components/navBar.js
@@ -1,11 +1,15 @@
 // appending the navBar in navBar-container
-document.querySelector(".navBar-container").innerHTML = navBar();
+// document.querySelector(".navbar-container").innerHTML = navBar();
 
-// function which will return the navBar in all pages necessary
-export function navBar() {
-  return `<nav class="navBar-content border-2 border-red-500 h-[4rem] w-full bg-white">
-            <div class="nav-logo border-2 border-green-500 h-full w-[5rem]">
-                <img class="h-full w-full" src="https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg"/>
-            </div>
-        </nav>`;
-}
+// // function which will return the navBar in all pages necessary
+// export function navBar() {
+//   return `<nav class="navbar-content">
+//             <div class="nav-logo">
+//                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZUrFzYr0WnOzsxDa4eXl38jjqx7Zk-PQeyA&s"/>
+//             </div>
+//             <div class="header">
+//                 <h3>Where, Around the World ?<h3>
+//                 <i class="lni lni-moon-half-right-5">Dark</i>
+//             </div>   
+//         </nav>`;
+// }
